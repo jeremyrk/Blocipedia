0b7f1066109c96219b01c43e9787c3f70a7484e9
Jeremy K <jeremy.rkw@gmail.com>
2013-11-24 19:50:04 -0800

    First commit & README update for Blocipedia

---
+++ README.md
@@ -0,0 +1,5 @@
+== README
+
+A Bloc version of Wikipedia, aka Blocipedia!
+
+More details coming soon...
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = '6c0a43def1941013e7617acd1c63f6b88c1508d7ec6a17a53b945ef04e9c27d171282547577d6bdcbe9068d2a7e24d5e374e840cfc2f7118aa65741fde729f90'
